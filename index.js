b128981b0384c74280b0bde2c6128593ca0083b9
timabloom <timabloom@proton.me>
2023-02-15 12:20:21 +0100

    created webpage & statistics get request + db.json

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+// @ts-nocheck
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const bodyParser = require("body-parser");
+const fs = require("fs");
 
 const app = express();
 const PORT = 4000;
@@ -10,13 +12,34 @@ app.use(cors());
 app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(express.static("build"));
 
 const corsOptions = {
   origin: "*",
 };
 
-app.get("/", cors(corsOptions), (req, res) => {
-  res.status(200).end();
+let statistics;
+
+jsonData("get");
+function jsonData(getData) {
+  fs.readFile("db.json", (err, data) => {
+    if (err) throw err;
+    else {
+      const parsedJson = JSON.parse(data);
+      if (getData === "get") {
+        statistics = parsedJson.jsonStatistics;
+      }
+    }
+  });
+}
+
+// Admin page
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./build", "index.html"));
+});
+
+app.get("/statistics", cors(corsOptions), (req, res) => {
+  res.status(200).json(statistics);
 });
 
 app.listen(PORT, () => {

7d3605e84afd6597232c1d04f4a251564e0d7074
timabloom <timabloom@proton.me>
2023-02-24 12:11:36 +0100

    added timestamp + readability

--- middlewares/error-logging.js
+++ middlewares/error-logging.js
@@ -3,8 +3,12 @@ const fs = require("fs");
 
 const logFile = fs.createWriteStream("server-error.log", { flags: "a" });
 
+const timestamp = new Date().toLocaleString();
+
 function errorLogging(error, path) {
-  logFile.write(`Error filepath: ${path}. Error message: ${error}. `);
+  logFile.write(
+    `Error:\nTimestamp: ${timestamp}.\nFilepath: ${path}.\nMessage: ${error}.\n\n`
+  );
 }
 
 module.exports = errorLogging;

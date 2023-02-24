866a1df110781e0a9eacb42efab01aedb375afe5
timabloom <timabloom@proton.me>
2023-02-24 12:26:01 +0100

    edited name

--- models/db.model.js
+++ models/db.model.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 // Read from mock db
 function readData() {
   return new Promise((resolve, reject) => {
-    fs.readFile("./db/db.json", (error, data) => {
+    fs.readFile("./database/db.json", (error, data) => {
       if (error) {
         reject(error);
       } else {
@@ -18,7 +18,7 @@ function readData() {
 // Write to mock db
 function writeData(stringifiedJson) {
   return new Promise((resolve, reject) => {
-    fs.writeFile("./db/db.json", stringifiedJson, (error) => {
+    fs.writeFile("./database/db.json", stringifiedJson, (error) => {
       if (error) {
         reject(error);
       } else {

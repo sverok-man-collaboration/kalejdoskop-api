8d0b526baa3119d3a4611ef3dd6ea8de51490ab4
timabloom <timabloom@proton.me>
2023-05-31 13:55:38 +0200

    automated adding the first user

--- index.ts
+++ index.ts
@@ -26,10 +26,7 @@ import {
 } from "helmet";
 import originCheck from "./middlewares/origin-apikey-check.mjs";
 import verifyToken from "./middlewares/authenticate-token.mjs";
-import encryptData from "./middlewares/encrypt-data.mjs";
-import errorLogging from "./middlewares/error-logging.mjs";
-import { fileURLToPath } from "url";
-const __filename = fileURLToPath(import.meta.url);
+import firstUser from "./middlewares/first-user.mjs";
 import pkg from "body-parser";
 const { urlencoded, json } = pkg;
 
@@ -45,27 +42,8 @@ import statisticsRoutes from "./routes/statistics.routes.mjs";
 // Generate key
 generateJWT();
 
-// Encrypt first time user data
-const email = process.env["SECRET_EMAIL"];
-const name = process.env["SECRET_NAME"];
-
-if (email && name) {
-  const { data, moreData, iv } = encryptData(email, name) || {};
-  if (data && moreData && iv) {
-    console.log(`Encrypted email: ${data}`);
-    console.log(`Encrypted name: ${moreData}`);
-    console.log(`IV: ${iv}`);
-  } else {
-    const errorMessage = "process.env.SECRET_KEY || iv is undefined";
-    console.log(errorMessage);
-    errorLogging(errorMessage, __filename);
-  }
-} else {
-  const errorMessage =
-    "process.env.SECRET_EMAIL and process.env.SECRET_NAME are undefined";
-  console.log(errorMessage);
-  errorLogging(errorMessage, __filename);
-}
+// Add first time user
+firstUser();
 
 const app = express();
 const PORT = 4000;
